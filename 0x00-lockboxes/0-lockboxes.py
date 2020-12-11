3b3930dcd68924a2e9620ee0891e1e90474b0d54
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 05:41:36 +0000

    Removed extraneous check in if statement

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,15 +2,13 @@
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
     unlocked = []
-    if len(boxes) == 0 or len(boxes[0]) == 0 or boxes is None:
+    if len(boxes) == 0 or len(boxes[0]) == 0:
         return False
 
     def unlocker(box):
         if box not in unlocked and box < len(boxes):
             unlocked.append(box)
-            # print(unlocked)
             for key in boxes[box]:
-                # print("Running unlocker({:d})".format(box))
                 unlocker(key)
 
     unlocker(0)
