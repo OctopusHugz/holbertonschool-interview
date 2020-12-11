b77c8ead688acb3268b4c0914f52c532d78d4fb7
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 05:44:37 +0000

    Changed if statements

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,9 +2,9 @@
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
     unlocked = []
-    if len(boxes) == 1:
+    if len(boxes) <= 1:
         return True
-    if len(boxes) == 0 or len(boxes[0]) == 0:
+    if len(boxes[0]) == 0:
         return False
 
     def unlocker(box):
