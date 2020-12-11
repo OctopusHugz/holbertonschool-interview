be0a298d6fc5d7f3cc963ebfb60e8eb98183eb2a
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 16:10:31 +0000

    Reverted to recursion and added check for all keys in boxes[0]

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,54 +1,24 @@
 #!/usr/bin/python3
-# global count
-# count = 0
-
-
-# def canUnlockAll(boxes):
-#     """This function checks to see if all lockboxes can be unlocked"""
-#     unlocked = []
-#     if len(boxes) <= 1:
-#         return True
-#     if len(boxes[0]) == 0:
-#         return False
-
-#     def unlocker(box, count):
-#         if box not in unlocked and box < len(boxes):
-#             unlocked.append(box)
-#             count += 1
-#             for key in boxes[box]:
-#                 print("Count is: {:d}".format(count))
-#                 unlocker(key, count)
-
-#     unlocker(0, count)
-#     if len(unlocked) == len(boxes):
-#         return True
-#     return False
-
-
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
-    unlocked = {0: True}
-    keys = []
-    index = 0
+    unlocked = []
+    first_key_check = []
     if len(boxes) <= 1:
         return True
     if len(boxes[0]) == 0:
         return False
-    for box in boxes:
-        for key in box:
-            if key not in keys:
-                keys.append(key)
-        if index + 1 in keys:
-            unlocked.update({index + 1: True})
-        else:
-            for k in keys:
-                if k == index + 1 or index + 1 in boxes[k]:
-                    unlocked.update({index + 1: True})
-                    break
-        index += 1
-    # print(unlocked)
-    # print(len(unlocked.keys()))
-    # print(len(boxes))
-    if len(unlocked.keys()) - 1 == len(boxes):
+    for key in boxes[0]:
+        first_key_check.append(key)
+    if len(first_key_check) == len(boxes):
+        return True
+
+    def unlocker(box):
+        if box not in unlocked and box < len(boxes):
+            unlocked.append(box)
+            for key in boxes[box]:
+                unlocker(key)
+
+    unlocker(0)
+    if len(unlocked) == len(boxes):
         return True
     return False
