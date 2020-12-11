0e1bf20049103ea4cc7afc240281cf87868eb673
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 03:53:51 +0000

    Updated solution to handle more cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
-    unlocked = []
-    keys = {}
+    unlocked = {0: True}
+    keys = []
     index = 0
-    if 1 not in boxes[0]:
-        return False
     for box in boxes:
         for key in box:
-            if key not in keys and key >= 1:
-                try:
-                    keys[index].append(key)
-                except KeyError:
-                    keys[index] = [key]
+            if key not in keys:
+                keys.append(key)
+        if index + 1 in keys:
+            unlocked.update({index + 1: True})
+        else:
+            for k in keys:
+                if k == index + 1 or index + 1 in boxes[k]:
+                    unlocked.update({index + 1: True})
+                    break
         index += 1
-    index = 0
-    for box in boxes:
-        if index + 1 in boxes[index]:
-            unlocked.append(True)
-        index += 1
-    if unlocked.count(True) == len(boxes) - 1:
+    if len(unlocked.keys()) == len(boxes):
         return True
     return False
