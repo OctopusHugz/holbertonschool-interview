35f4e1f3720ff14a03a26077603f44046719778f
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 04:54:07 +0000

    How long can you bang your head up against recursion before you realize it's your answer? Updated solution to be much simpler and cleaner using recursion. Need to remove print statements if valid solution

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
-    unlocked = {0: True}
-    keys = []
-    index = 0
-    for box in boxes:
-        for key in box:
-            if key not in keys:
-                keys.append(key)
-        if index + 1 in keys:
-            unlocked.update({index + 1: True})
-        else:
-            for k in keys:
-                if k == index + 1 or index + 1 in boxes[k]:
-                    unlocked.update({index + 1: True})
-                    break
-        index += 1
-    if len(unlocked.keys()) == len(boxes):
+    unlocked = []
+
+    def unlocker(box):
+        if box not in unlocked:
+            unlocked.append(box)
+            # print(unlocked)
+            for key in boxes[box]:
+                # print("Running unlocker({:d})".format(box))
+                unlocker(key)
+
+    unlocker(0)
+    if len(unlocked) == len(boxes):
         return True
     return False
