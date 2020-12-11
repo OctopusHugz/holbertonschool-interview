58c27e00a2af967fedb2c853843a1c6df40f7475
OctopusHugz <colsonscott53@gmail.com>
2020-12-11 04:57:28 +0000

    Added logic to handle edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,14 +2,19 @@
 def canUnlockAll(boxes):
     """This function checks to see if all lockboxes can be unlocked"""
     unlocked = []
+    if 1 not in boxes[0]:
+        return False
 
     def unlocker(box):
         if box not in unlocked:
             unlocked.append(box)
             # print(unlocked)
-            for key in boxes[box]:
-                # print("Running unlocker({:d})".format(box))
-                unlocker(key)
+            try:
+                for key in boxes[box]:
+                    # print("Running unlocker({:d})".format(box))
+                    unlocker(key)
+            except IndexError:
+                pass
 
     unlocker(0)
     if len(unlocked) == len(boxes):
