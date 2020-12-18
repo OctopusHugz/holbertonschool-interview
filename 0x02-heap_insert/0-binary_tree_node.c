ee77e0be755d9fe512b8eea4f8fe8cad9919200a
OctopusHugz <colsonscott53@gmail.com>
2020-12-18 21:28:08 +0000

    Added return to function documentation

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,6 +5,7 @@
  *
  * @parent: Pointer to the parent node
  * @value: Integer stored in the node
+ * Return: pointer to the created node, or NULL on failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
