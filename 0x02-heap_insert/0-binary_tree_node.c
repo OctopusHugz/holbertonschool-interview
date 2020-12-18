19b5927128c3429e5ef887eb2009d126c9040d70
OctopusHugz <colsonscott53@gmail.com>
2020-12-18 21:25:57 +0000

    Added solution for exercise 0 and header file

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ *
+ * @parent: Pointer to the parent node
+ * @value: Integer stored in the node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t) * 1);
+
+	if (!node)
+		return NULL;
+	node->n = value;
+	node->parent = NULL;
+	node->left = NULL;
+	node->right = NULL;
+	if (parent)
+		node->parent = parent;
+	return (node);
+}
