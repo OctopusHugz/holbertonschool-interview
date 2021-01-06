3907291784c85aeefdc9d58806a83c73f4f8ef66
OctopusHugz <colsonscott53@gmail.com>
2021-01-06 21:44:07 +0000

    Added next iteration to find primes

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,30 +2,33 @@
 """This module returns minimum operations"""
 
 
+# def minOperations(n):
+#     """Returns minimum operations to result in n characters in a text file"""
+#     if n < 1:
+#         return 0
+#     else:
+#         return n
+
+
 def minOperations(n):
     """Returns minimum operations to result in n characters in a text file"""
-    if n < 1:
-        return 0
-    else:
-        return n
+    # find smallest divisor starting at 2
+    # if no divisor found, it's prime number
+    sd = smallest_divisor(n)
+    if sd == n:
+        # prime number
+        return sd
+    count = 1
+    for i in range(sd, n + 1, sd):
+        if i == n:
+            return count
+        count += 1
+    return count
 
 
-# def minOperations_full(n):
-#     # find smallest divisor starting at 2
-#     # if no divisor found, it's prime number
-#     sd = smallest_divisor(n)
-#     if sd == n:
-#         print("{} is a prime number!".format(n))
-#     count = 1
-#     for i in range(sd, n + 1, sd):
-#         if i == n:
-#             return count
-#         count += 1
-#     return count
-
-
-# def smallest_divisor(n):
-#     for i in range(2, n):
-#         if n % i == 0:
-#             return i
-#     return n
+def smallest_divisor(n):
+    """Returns smallest divisor"""
+    for i in range(2, n):
+        if n % i == 0:
+            return i
+    return n
