95b37f65068ff67a9e6a4afd9ace2d983e4d2615
OctopusHugz <colsonscott53@gmail.com>
2021-01-06 23:46:54 +0000

    Trying factoring route

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -40,6 +40,14 @@ def smallest_divisor(n):
     return n
 
 
+def highest_divisor(n):
+    """Returns highest divisor"""
+    for i in range(2, n):
+        if n % i == 0:
+            return n // i
+    return n
+
+
 def greater_divisor(n, sd):
     """Returns False if no greater divisor, True if yes"""
     for i in range(sd + 1, n + 1):
@@ -48,33 +56,63 @@ def greater_divisor(n, sd):
     return False
 
 
-# start at 0
-# as you add characters, see if your character count is a divisor of n
-# if it is, copy all
-# else, paste
-
-
 def minOperations(n):
     """Returns minimum operations to result in n characters in a text file"""
     min_ops = 0
-    step = 2
-    sd = smallest_divisor(n)
-    gd = greater_divisor(n, sd)
+    # sd = smallest_divisor(n)
+    # hd = highest_divisor(n)
     # print("sd is: {}".format(sd))
-    # print("gd is: {}".format(gd))
-    if n < 1:
-        return 0
-    for i in range(sd, n + 1, step):
-        # print("i is: {}".format(i))
-        # print("step is: {}".format(step))
-        # print("min_ops is: {}".format(min_ops))
-        if n % i == 0:
-            min_ops += step
-        else:
-            step += 1
-        # print("i is now: {}".format(i))
-        # print("step is now: {}".format(step))
-        # print("min_ops is now: {}".format(min_ops))
-        if min_ops > 0 and n % min_ops == 0 and gd is True and min_ops != sd:
-            return min_ops + (n // min_ops)
-    return min_ops
+    # print("hd is: {}".format(hd))
+    # if n < 1:
+    #     return 0
+    # for i in range(2, n + 1):
+    #     print("i is: {}".format(i))
+    #     print("min_ops is: {}".format(min_ops))
+    #     while n % i != 0:
+    #         min_ops += i
+    #         n //= i
+    #     print("min_ops is now: {}".format(min_ops))
+    # return min_ops
+
+
+    def factors(n):
+        d = 2
+        while d * d <= n:
+            while n % d == 0:
+                n /= d
+                yield d
+            d += 1
+        if n > 1:
+            yield n
+    return int(sum(factors(n)))
+
+# def minOperations(n):
+#     """Returns minimum operations to result in n characters in a text file"""
+#     min_ops = 0
+#     step = 2
+#     sd = smallest_divisor(n)
+#     hd = highest_divisor(n)
+#     gd = greater_divisor(n, sd)
+#     print("sd is: {}".format(sd))
+#     print("hd is: {}".format(hd))
+#     # print("gd is: {}".format(gd))
+#     if n < 1:
+#         return 0
+#     for i in range(sd, n + 1, step):
+#         print("i is: {}".format(i))
+#         print("step is: {}".format(step))
+#         print("min_ops is: {}".format(min_ops))
+#         if n % i == 0:
+#             min_ops += step
+#         else:
+#             step += 1
+#         print("i is now: {}".format(i))
+#         print("step is now: {}".format(step))
+#         print("min_ops is now: {}".format(min_ops))
+#         if min_ops > 0 and n % min_ops == 0 and gd is True and min_ops != sd:
+#             # if doubling the min_ops/character_count is a factor of n
+#             if n % (min_ops * 2) == 0:
+#                 min_ops *= 2
+#             elif min_ops == hd:
+#                 return min_ops + (n // min_ops)
+#     return min_ops
