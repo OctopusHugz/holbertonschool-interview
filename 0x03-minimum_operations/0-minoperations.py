ed0f20c496d236d53561e7c3272c1dfa799984b1
OctopusHugz <colsonscott53@gmail.com>
2021-01-06 21:37:29 +0000

    Comment out other functions

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,22 +6,22 @@ def minOperations(n):
         return n
 
 
-def minOperations_full(n):
-    # find smallest divisor starting at 2
-    # if no divisor found, it's prime number
-    sd = smallest_divisor(n)
-    if sd == n:
-        print("{} is a prime number!".format(n))
-    count = 1
-    for i in range(sd, n + 1, sd):
-        if i == n:
-            return count
-        count += 1
-    return count
+# def minOperations_full(n):
+#     # find smallest divisor starting at 2
+#     # if no divisor found, it's prime number
+#     sd = smallest_divisor(n)
+#     if sd == n:
+#         print("{} is a prime number!".format(n))
+#     count = 1
+#     for i in range(sd, n + 1, sd):
+#         if i == n:
+#             return count
+#         count += 1
+#     return count
 
 
-def smallest_divisor(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return i
-    return n
+# def smallest_divisor(n):
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return i
+#     return n
