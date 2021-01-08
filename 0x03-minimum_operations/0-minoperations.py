8a1c5159870673b37f1a087ad8be80fba508e787
OctopusHugz <colsonscott53@gmail.com>
2021-01-08 05:24:43 +0000

    Changed factors_list to total_ops for readability/flow

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,13 +4,13 @@
 
 def minOperations(n):
     """Returns minimum operations to result in n characters in a text file"""
-    factors_list = []
+    total_ops = []
     step = 2
     while step * step <= n:
         while n % step == 0:
             n /= step
-            factors_list.append(int(step))
+            total_ops.append(int(step))
         step += 1
     if n > 1:
-        factors_list.append(int(n))
-    return sum(factors_list)
+        total_ops.append(int(n))
+    return sum(total_ops)
