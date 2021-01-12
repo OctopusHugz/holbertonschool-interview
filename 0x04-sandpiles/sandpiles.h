e965eaf4eea63d11391af54c3c6767ecd4b52556
OctopusHugz <colsonscott53@gmail.com>
2021-01-12 23:08:35 +0000

    Deleted outline for second version and updated header file with only prototype for 1 pile version

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -10,6 +10,4 @@ int is_stable_sandpile(int grid1[3][3]);
 
 void topple(int grid1[3][3], int topple_count);
 
-/* void topple(int grid1[3][3], int newpiles[3][3]); */
-
 static void print_grid(int grid[3][3]);
