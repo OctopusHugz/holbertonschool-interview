1de30b8545ef8be6bcecf7fa1f03f8b397facf51
OctopusHugz <colsonscott53@gmail.com>
2021-01-14 16:14:43 +0000

    Added include guard to header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,3 +1,6 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,3 +14,5 @@ int is_stable_sandpile(int grid1[3][3]);
 void topple(int grid1[3][3], int topple_count);
 
 static void print_grid(int grid[3][3]);
+
+#endif SANDPILES_H
