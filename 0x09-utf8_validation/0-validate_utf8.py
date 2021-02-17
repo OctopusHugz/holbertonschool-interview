b443c75f78adbb3ba7e87bbfe6bdf778b721590b
OctopusHugz <colsonscott53@gmail.com>
2021-02-17 00:22:03 +0000

    Added solution for ASCII or 4 character encodings. Need to make dynamic

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,12 +5,18 @@
 def validUTF8(data):
     """Determines if given data set represents valid UTF-8 encoding and returns
     true if data set is valid UTF-8, otherwise returns false"""
-    # A character in UTF-8 can be 1 to 4 bytes long
-    # The data set can contain multiple characters
-    # The data will be represented by a list of integers
-    # Each integer represents 1 byte of data, therefore you only need to
-    # handle the 8 least significant bits of each integer
-    for num in data:
-        if num > 255:
-            return False
+    for i in range(len(data)):
+        num = data[i]
+        if num >= 0 and num <= 127:
+            continue
+        else:
+            if num >= 194 and num <= 253:
+                # Need to dynamically determine length in bytes (not always 3!)
+                next_three = data[i+1:i+4]
+                # Check next 3 integers are between 128-191
+                for number in next_three:
+                    if number < 128 or number > 191:
+                        return False
+            else:
+                return False
     return True
