d9c88f0729987aba5699c6e24010fc0311255b12
OctopusHugz <colsonscott53@gmail.com>
2021-02-22 16:00:11 +0000

    Added outline for 0-slide_line.c

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,13 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: array of integers containing size number of elements
+ * @size: number of elements in line array
+ * @direction: direction to slide and merge integers
+ * Return: 1 if success, 0 if failure
+ **/
+
+int slide_line(int *line, size_t size, int direction)
+{
+}
