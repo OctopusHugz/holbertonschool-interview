d2e19fc47d504d240682f03da2b01797e2de565f
OctopusHugz <colsonscott53@gmail.com>
2021-02-22 15:58:10 +0000

    Added slide_line.h

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#define SLIDE_LEFT "left"
+#define SLIDE_RIGHT "right"
+
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_LINE_H */
