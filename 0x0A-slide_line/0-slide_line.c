86c7e56a1b7b78b54f2a74c71a43b54cc57c0361
OctopusHugz <colsonscott53@gmail.com>
2021-02-24 06:19:06 +0000

    Playing with return values

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -12,8 +12,6 @@ int slide_line(int *line, size_t size, int direction)
 {
 	size_t i, next_index;
 
-	if (size < 1)
-		return (0);
 	/* SLIDE AND MERGE LEFT */
 	if (direction == -1)
 	{
@@ -39,7 +37,7 @@ int slide_line(int *line, size_t size, int direction)
 		}
 	}
 	else
-		return (0);
+		return (1);
 	return (1);
 }
 
