e968cde4fbd6ff020556c4854c59c922eb931aff
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 18:54:23 +0000

    Testing hardcode of 4

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -13,3 +13,7 @@ else:
     if n < 4:
         print("N must be at least 4")
         exit(1)
+
+if n == 4:
+    print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+    print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
