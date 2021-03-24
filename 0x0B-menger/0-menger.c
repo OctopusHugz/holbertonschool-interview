372bc6cc055bd8a48ca7a672c57298fef5f841ef
OctopusHugz <colsonscott53@gmail.com>
2021-03-24 16:59:57 +0000

    Added new logic for width

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -7,10 +7,14 @@
 
 void menger(int level)
 {
+	int width = pow(3, level);
+
+	/* Each subsequent level multiplies cube by 3 with middle cube empty */
 	if (level == -1)
 		return;
 	else if (level == 0)
 		printf("#\n");
 	else if (level == 1)
 		printf("###\n# #\n###\n");
+	printf("%d\n", width);
 }
