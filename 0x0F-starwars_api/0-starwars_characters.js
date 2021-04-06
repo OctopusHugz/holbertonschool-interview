72c2b700ef215215cc7e67445302cf4ef8766ec6
OctopusHugz <colsonscott53@gmail.com>
2021-04-06 19:19:46 +0000

    Added 0-starwars_characters.js

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require("request");
+const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+function getCharacter(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      resolve(body);
+    });
+  });
+}
+
+async function printFunc(url) {
+  const body = await getCharacter(url);
+  console.log(JSON.parse(body).name);
+}
+request(url, async function (error, response, body) {
+  if (error) console.error("error:", error);
+  const characters = JSON.parse(body).characters;
+  for (let index = 0; index < characters.length; index++) {
+    const characterUrl = characters[index];
+    await printFunc(characterUrl);
+  }
+});
