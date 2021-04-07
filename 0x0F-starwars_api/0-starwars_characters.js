4dfb543d505353f11a38559b5e63d468aea3717f
OctopusHugz <colsonscott53@gmail.com>
2021-04-07 06:29:43 +0000

    Semistandard fixes

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
-function getCharacter(url) {
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+function getCharacter (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) reject(error);
@@ -10,12 +10,12 @@ function getCharacter(url) {
   });
 }
 
-async function printFunc(url) {
+async function printFunc (url) {
   const body = await getCharacter(url);
   console.log(JSON.parse(body).name);
 }
 request(url, async function (error, response, body) {
-  if (error) console.error("error:", error);
+  if (error) console.error('error:', error);
   const characters = JSON.parse(body).characters;
   for (let index = 0; index < characters.length; index++) {
     const characterUrl = characters[index];
