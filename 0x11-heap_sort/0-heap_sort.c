a387a2989e65b41786d282aecfbcd38aa1a096f0
OctopusHugz <colsonscott53@gmail.com>
2021-04-22 23:49:58 +0000

    Fixed args to print_array

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -8,26 +8,5 @@
 
 void heap_sort(int *array, size_t size)
 {
-	print_array(array);
-}
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
+	print_array(array, size);
 }
