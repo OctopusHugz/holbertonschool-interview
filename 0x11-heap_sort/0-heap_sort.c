89c1be55bee0f6ea8b15ed52e0cf69aab1db71d5
OctopusHugz <colsonscott53@gmail.com>
2021-04-23 03:15:46 +0000

    Added more logic

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -8,8 +8,18 @@
 
 void heap_sort(int *array, size_t size)
 {
+	int temp;
+
 	if (size == 0 || !array)
 		return;
-	if (size > 1 && (array[0] > array[1] || array[0] == array[size - 1]))
+	if (size == 2)
+	{
+		temp = array[1];
+		array[1] = array[0];
+		array[0] = temp;
+		print_array(array, size);
+		return;
+	}
+	if (array && size > 1 && (array[0] > array[1] || array[0] != array[size - 1]))
 		print_array(array, size);
 }
