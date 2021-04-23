edbada42e01cb1d2fff3e31e3254be7b985a37e2
OctopusHugz <colsonscott53@gmail.com>
2021-04-23 03:29:58 +0000

    Added incomplete outline

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,4 +4,18 @@
 
 def rain(walls):
     """ Calculates how much rain water is retained """
-    return sum(walls[1:-1])
+    total_rain = 0
+    for i in range(len(walls)):
+        index = walls[i]
+        if i > 0:
+            left_list = walls[0:i]
+            left_wall_height = max(left_list)
+        else:
+            left_wall_height = index
+        if i < len(walls):
+            right_list = walls[i:]
+            right_wall_height = max(right_list)
+        else:
+            right_wall_height = index
+        total_rain += (min(left_wall_height, right_wall_height) - index)
+    return total_rain
