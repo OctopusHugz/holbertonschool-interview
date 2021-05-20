62b7f56ba0c8f08ed3ba0122b11b658253aa6b88
OctopusHugz <colsonscott53@gmail.com>
2021-05-20 18:23:33 +0000

    Added betty fixes

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -72,25 +72,29 @@ void _puts(char *str)
 	_putchar('\n');
 }
 
+/**
+ * itoa - turns integer to string
+ * @n: integer to turn to string
+ * @s: array to store string
+ */
 
- void itoa(int n, char s[])
- {
-     int i, sign;
+void itoa(int n, char s[])
+{
+	int i, sign = n;
 
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
- }
+	if (sign < 0) /* record sign */
+		n = -n;			/* make n positive */
+	i = 0;
+	do {						   /* generate digits in reverse order */
+		s[i++] = n % 10 + '0'; /* get next digit */
+	} while ((n /= 10) > 0);   /* delete it */
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
 
-
- /**
+/**
  * _strlen - returns the length of a string
  * @s: String to return the lenght of
  *
@@ -110,14 +114,20 @@ int _strlen(char *s)
 	_putchar('\n');
 }
 
-void reverse(char s[])
- {
-     int i, j;
-     char c;
 
-     for (i = 0, j = _strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
- }
+/**
+ * reverse - reverse an array
+ * @s: array to reverse
+ */
+void reverse(char s[])
+{
+	int i, j;
+	char c;
+
+	for (i = 0, j = _strlen(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
