f5e62e721ee440d925533ef8204ae0d8fa226298
OctopusHugz <colsonscott53@gmail.com>
2021-07-06 18:23:34 +0000

    Added outline for all files

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * radix_sort - sorts an array of integers in ascending order using Radix sort
+ * @array: array to sort
+ * @size: size of array to sort
+ **/
+
+void radix_sort(int *array, size_t size)
+{
+  if (!array || size < 2)
+    return;
+}
