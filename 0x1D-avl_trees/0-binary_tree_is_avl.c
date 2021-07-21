fac5d4fa9a71ea8ec1738d9e87cdb6e3683a8ba5
OctopusHugz <colsonscott53@gmail.com>
2021-07-21 17:37:07 +0000

    Refactored is_BST to simplify logic

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -78,13 +78,9 @@ int is_BST(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (tree->left && tree->left->n >= tree->n)
+	if (tree->left && (tree->left->n >= tree->n || !is_BST(tree->left)))
 		return (0);
-	if (tree->right && tree->right->n <= tree->n)
-		return (0);
-	if (tree->left && !is_BST(tree->left))
-		return (0);
-	if (tree->right && !is_BST(tree->right))
+	if (tree->right && (tree->right->n <= tree->n || !is_BST(tree->right)))
 		return (0);
 	return (1);
 }
