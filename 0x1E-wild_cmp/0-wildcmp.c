10af505b0dd18c7e7a6c514c326fd7c85d755064
OctopusHugz <colsonscott53@gmail.com>
2021-07-26 19:08:40 +0000

    Added attempt at clean answer with convolution in comments

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -3,20 +3,34 @@
 /**
  * wildcmp - compares two strings to check whether identical
  * @s1: first string
- * @s2: second string
+ * @s2: second string that can contain the special character *
  *
  * Return: 1 if considered identical, 0 otherwise
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (strcmp(s2, "h.*c") == 0)
-		return (0);
-	else if (strcmp(s2, "holberton*d") == 0)
-		return (0);
-	else if (strcmp(s2, "*e") == 0)
-		return (0);
-	else if (strcmp(s1, "main.c") == 0)
-		return (0);
-	return (1);
+	/* Base Case: we've reached the end of both strings, thus it's a match */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	/* Exact character match: move forward 1 char in each string */
+	else if (*s1 == *s2)
+		return wildcmp(s1 + 1, s2 + 1);
+	/* Wildcard match in s2: need to check further */
+	else if (*s2 == '*')
+	{
+		/* printf("s1: %c\ns2: %c\n\n", *s1, *s2);
+		printf("s1: %c\ns2 + 1: %c\n\n", *s1, *(s2 + 1));
+
+		if (*s1 != '\0' && (*s1 == *(s2 + 1) || *(s2 + 1) == '*'))
+			return wildcmp(s1, s2 + 1);
+		else if (*s1 != '\0' && *(s1 + 1) != *(s2 + 1))
+			return wildcmp(s1 + 1, s2);
+		else if (*s1 != '\0' && *(s1 + 1) == *(s2 + 1))
+			return wildcmp(s1 + 1, s2 + 1);
+		return wildcmp(s1, s2 + 1); */
+		return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
+	}
+	/* No match */
+	return (0);
 }
