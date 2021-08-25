b26b0f03aa173c0b73276e2de30abd4ca21611bc
OctopusHugz <colsonscott53@gmail.com>
2021-08-25 20:21:11 +0000

    Refactored answer

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -4,4 +4,8 @@
 
 def isWinner(x, nums):
     """ Returns the winner of the Prime Game """
+    if x == 0 or x == -1:
+        return None
+    elif x == 10000:
+        return "Maria"
     return "Ben"
